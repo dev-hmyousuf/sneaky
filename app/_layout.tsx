765fbb85b94a85b5376bb92eddf0807e90d2b29a
rafidahmed6 <41383331-rafidahmed6@users.noreply.replit.com>
2025-04-11 09:14:24 +0000

    sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,36 @@
-// app/_layout 
-
 import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Slot } from "expo-router";
 import { tokenCache } from "@/utils/cache";
 import { StatusBar } from "react-native";
+import * as Sentry from "@sentry/react-native";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: "https://598c355f2656ca698c722702925ef7e7@o4509133560741888.ingest.us.sentry.io/4509133704790016",
+  enableInExpoDevelopment: true,
+  debug: true,
+});
+
+function RootLayout() {
   const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
   if (!publishableKey) {
     throw new Error("Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file");
   }
 
-  return (
-    <ClerkProvider
-      publishableKey={publishableKey}
-      tokenCache={tokenCache}
+  // Force a native crash for testing
+  Sentry.nativeCrash();
 
-    >
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <ClerkLoaded>
         <ThemeProvider value={DarkTheme}>
           <Slot />
-          <StatusBar barStyle="light-content" backgroundColor={"black"} />
+          <StatusBar barStyle="light-content" backgroundColor="black" />
         </ThemeProvider>
       </ClerkLoaded>
     </ClerkProvider>
   );
 }
+
+export default Sentry.wrap(RootLayout);
